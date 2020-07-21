31e459ba8effd16e3a2854be3b5b9ea99387c2ed
刘电波 <liudianbo@aobi.com>
2020-07-21 16:15:05 +0800

    MOD <导表> 日志待优化

--- src/main/java/com/baitian/autotable/ClientApplication.java
+++ src/main/java/com/baitian/autotable/ClientApplication.java
@@ -3,15 +3,13 @@ package com.baitian.autotable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileNotFoundException;
-
 /**
  * @author liudianbo
  */
 @SpringBootApplication
 public class ClientApplication {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 }
